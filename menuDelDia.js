89a411c4a2e406ad332aaf24399bcde7b46ea921
marc <2005marcmc@gmail.com>
2025-04-03 16:04:13 +0200

    Exercici Menú 03/04/2025 16:04

---
+++ menuDelDia.js
@@ -0,0 +1,12 @@
+
+function primerPlat() {
+    document.getElementById("imatge").src = "/images/pesols.jpg";
+}
+
+function segonPlat() {
+    document.getElementById("imatge").src = "/images/fettuccini-funghi.jpg";
+}
+
+function postre() {
+    document.getElementById("imatge").src = "/images/donuts.jpg";
+}
